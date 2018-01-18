1cb06bcf403bc6b464ed280426f7d6159227a6be
Andrei Lazar <l.andrei37@yahoo.ro>
2018-01-18 23:55:57 +0200

    Updated Form.

--- Site-minified/php/parser.php
+++ Site-minified/php/parser.php
@@ -1,32 +1,28 @@
-<?php
-if( isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['telephone']) && isset($_POST['email']) && isset($_POST['message']) ){
-	$fname = $_POST['fname']; // HINT: use preg_replace() to filter the data
-    $lname = $_POST['lname'];
-    $telephone = $_POST['telephone'];
-    $email = $_POST['email'];
-	$message = nl2br($_POST['message']);
-	$to = "scorppion2005@yahoo.com";	
-	$from = $email;
-    $subject = 'S-a completat un formular pe AndreiLazar.Net .';
-	$message = '<b>fname:</b> '.$fname.' '.$lname.' <br><b>Mail:</b> '.$email.' <br><b>Tipul firmei:</b> '.$tipfirma.' <p><b>Comentarii: </b>'.$comentarii.'</p>';
-  
-    $message = '<html><body>';
-    $message .= '<img src="http://milie37.000webhostapp.com/img/MihaelaIlieLogoMic.jpg" alt="Mihaela Ilie" />';
-    $message .= '<table rules="all" style="border: solid 3px #253340; border-radius: 2px; border-spacing: 0;" cellpadding="10">';
-    $message .= "<tr><td  style='background: #E5E5E5; color: #081727; padding: 2px;'><strong>fname:</strong> </td><td style='background: #081727; color: #E5E5E5; padding: 2px;'>".$fname." ".$lname."</td></tr>";
-    $message .= "<tr><td  style='background: #E5E5E5; color: #081727; padding: 2px;'><strong>Numar de telefon:</strong> </td><td style='background: #081727; color: #E5E5E5; padding: 2px;'>".$nrtel."</td></tr>";
-    $message .= "<tr><td  style='background: #E5E5E5; color: #081727; padding: 2px;'><strong>Mail:</strong> </td><td style='background: #081727; color: #E5E5E5; padding: 2px;'>".$email."</td></tr>";
-    $message .= "<tr><td  style='background: #E5E5E5; color: #081727; padding: 2px;'><strong>Tipul de firma:</strong> </td><td style='background: #081727; color: #E5E5E5; padding: 2px;'>".$tipfirma."</td></tr>";
-    $message .= "<tr><td  style='background: #E5E5E5; color: #081727; padding: 2px;'><strong>Comentarii:</strong> </td><td style='background: #081727; color: #E5E5E5; padding: 2px;'>".$comentarii."</td></tr>";
-    $message .= "</table>";
-    $message .= "</body></html>";
-	$headers = "From: $from\n";
-	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
-	if( mail($to, $subject, $messagemail, $headers) ){
-		echo "Succes";
-	} else {
-		echo "Mesajul nu s-a putut trimite. Va rugam incercati mai tarziu.";
-	}
-}
+<?php
+	$fname = !empty($_POST['fname']) ? $_POST['fname'] : 'Null';
+    $lname = !empty($_POST['lname']) ? $_POST['lname'] : 'Null';
+    $telephone = !empty($_POST['telephone']) ? $_POST['telephone'] : 'Null';
+    $email = !empty($_POST['email']) ? $_POST['email'] : 'Null';
+	$message = !empty($_POST['message']) ? nl2br($_POST['message']) : 'Null';
+    $to = "scorppion2005@yahoo.com";	
+	$from = $email;
+    $subject = 'S-a completat un formular pe AndreiLazar.Net .';
+    
+    $headers  = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+    $headers .= "From: ". $from. "\r\n";
+    $headers .= "Reply-To: ". $from. "\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+    $headers .= "X-Priority: 1" . "\r\n"; 
+
+    echo ($fname + " ");
+    echo ($lname + " ");
+    echo ($email + " ");
+    
+if( mail('andreimhlazar@gmail.com', $subject, $message, $headers) ){
+		echo "Succes";
+	} else {
+		echo "Mesajul nu s-a putut trimite. Va rugam incercati mai tarziu.";
+	}
+
 ?>
